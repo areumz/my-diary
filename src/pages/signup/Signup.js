8a149675091a636c513261bb17b7a9dde3bbcd76
areumz <ksah1016@naver.com>
2022-07-12 15:52:17 +0900

    Feat: email validation check

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -8,6 +8,24 @@ export default function Signup() {
   const [displayname, setDisplayname] = useState("");
   const { error, isPending, signup } = useSignup();
 
+  const [emailMsg, setEmailMsg] = useState("");
+  const [isEmail, setIsEmail] = useState(false);
+
+  const handleEmail = useCallback((event) => {
+    const emailRegex =
+      /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
+    const currentEmail = event.target.value;
+    setEmail(currentEmail);
+
+    if (!emailRegex.test(currentEmail)) {
+      setEmailMsg("올바른 이메일 형식이 아닙니다");
+      setIsEmail(false);
+    } else {
+      setIsEmail(true);
+      setEmailMsg("");
+    }
+  }, []);
+
   const handleData = (event) => {
     if (event.target.type === "password") {
       setPassword(event.target.value);
@@ -35,8 +53,11 @@ export default function Signup() {
           id="myEmail"
           required
           value={email}
-          onChange={handleData}
+          onChange={handleEmail}
         ></input>
+        {email.length > 0 && (
+          <span className={styles.email_error}>{emailMsg}</span>
+        )}
 
         <label htmlFor="myPassword">password(비밀번호): </label>
         <input
